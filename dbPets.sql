2256b663a020d986e3fa810af2c501de89eb6cb4
unknown <joaopaulovieirasilva9@gmail.com>
2024-08-02 19:49:33 -0300

    script

--- dbPets.sql
+++ dbPets.sql
@@ -6,4 +6,22 @@ create database dbPets;
 use dbPets;
 -- visializar banco de dados
 show databases;
+-- criando tabelas no banco de dados
+create table tbUsuarios(
+codUsu int not null alto_increment,
+nome varchar(50) not null,
+senha varchar(10) not null,
+primary key(codUsu)
+);
 
+create table tbFuncionarios(
+codFunc int not null auto_increment,
+nome varchar(100) not null,
+email varchar(100),
+telCel char(10)
+);
+
+-- visualizando tabelas
+show tables;
+-- visualizando a estrutura da tebela
+desc tbUsuarios;
