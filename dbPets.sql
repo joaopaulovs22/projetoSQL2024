2989b6cfa01546cff87c16982f194a667914f7a5
unknown <joaopaulovieirasilva9@gmail.com>
2024-08-02 19:28:26 -0300

    script sql pets

---
+++ dbPets.sql
@@ -0,0 +1,9 @@
+-- apagar banco de dados 
+drop database dbPets;
+-- Criar banco de dados
+create database dbPets;
+--acessar o banco de dados
+use dbPets;
+-- visializar banco de dados
+show databases;
+
